e3d7923a9122a19b2a6862500358ec1d58a4fa4d
Kent Andersen <kentareandersen@gmail.com>
2015-08-12 18:02:53 +0200

    oppdaterte presentasjon til å bruke bekk/remark

--- remark/init.js
+++ remark/init.js
@@ -1,11 +1,8 @@
 var slideshow = remark.create({
-  highlightStyle: 'solarized_light',
+  ratio: '16:9',
+  highlightStyle: 'monokai',
   highlightLanguage: 'javascript',
   navigation: {
     scroll: false
   }
-});
-
-Array.prototype.forEach.call(document.querySelectorAll(".remark-slide-content h1"), function(el) {
-  el.innerHTML = "<span>"+el.innerHTML+"</span>";
 });
